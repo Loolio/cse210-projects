78b626c3de76f8abc3a773b145be903724dc805a
Loolio <ploolio@byui.edu>
2025-06-16 12:04:46 +0300

    Shapes program

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -5,5 +5,30 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the EternalQuest Project.");
+
+        List<Shape> shapes = new List<Shape>();
+
+        // We create indvidual shapes and add them to the list 
+
+        Square square = new Square("Red", 3);
+        shapes.Add(square);
+
+        Rectangle rectangle = new Rectangle("Blue", 4, 5);
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle("Green", 6);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            // Notice that all shapes have a GetColor method from the base class
+            string color = shape.GetColor();
+
+            // Notice that all shapes have a GetArea method, but the behavior is
+            // different for each type of shape
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
